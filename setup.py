0eb2deb76a4a0dd648b3fdd3049f5b1b663e93de
Yamada <ymd_h@outlook.jp>
2024-04-07 16:39:04 +0900

    Add dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(name="nbds",
       version="0.0.0",
       description="Notebook Data Scientist",
       packages=find_packages(),
-      install_requires=["typing_extensions", "well-behaved-logging"],
+      install_requires=["typing_extensions", "well-behaved-logging", "ipython"],
       extras_require={
+          "jupyter": ["jupyter"],
           "gemini": ["google-generativeai"],
       })
