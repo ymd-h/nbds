81041ac500284c70468aab3114756b16b77dbe02
Yamada <ymd_h@outlook.jp>
2024-04-09 21:16:40 +0900

    Update packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os
 from setuptools import setup, find_packages
+
+with open("./README.md") as f:
+    long_description = f.read()
 
 setup(name="nbds",
       version="0.0.0",
       description="Notebook Data Scientist",
+      long_description = long_description,
+      long_description_content_type = "text/markdown",
       packages=find_packages(),
       install_requires=["typing_extensions", "well-behaved-logging", "ipython"],
       extras_require={
